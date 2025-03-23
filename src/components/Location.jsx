1e76959befc661580bc2dd7752b9176814961508
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-03-23 23:59:41 +0530

    Update Location.jsx

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Location = () => {
   return (
-    <div>
-      <div className="max-w-[1540px] mx-auto h-[500px] flex justify-center items-center py-8 mt-4">
+    <div className="bg-[#007BFF] py-10 px-6 rounded-xl shadow-xl mt-8">
+      <h2 className="text-center text-2xl font-bold text-white mb-6">
+        Our Location
+      </h2>
+      <div className="max-w-[1200px] mx-auto flex justify-center items-center rounded-lg overflow-hidden shadow-lg">
         <iframe
-          className="w-full"
-          height={"450px"}
-          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Dhaka%20Bangladesh+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          className="w-full h-[400px] rounded-lg"
+          src="https://maps.google.com/maps?q=VIT%20Bhopal%20University,%20India&output=embed"
           title="Google Map"
-        >
-          <Link to="https://www.gps.ie/">gps vehicle tracker</Link>
-        </iframe>
+        ></iframe>
       </div>
     </div>
   );
